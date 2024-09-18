fc968bb8448c37cb7475ff4054d7bd388fa9e976
pedrohpiress <pedro.psouza@sptech.school>
2024-09-18 14:22:57 -0300

    Alguns desafios concluídos

--- Alura/screenmatch/src/Principal.java
+++ Alura/screenmatch/src/Principal.java
@@ -1,16 +1,57 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculo.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.incluidoNoPlano = true;
+        meuFilme.setNome("O Poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setIncluidoNoPlano(true);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duracao do filme " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibirFichaTecnica();
         meuFilme.avalia(8.0);
         meuFilme.avalia(9.0);
         meuFilme.avalia(10.0);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
-        System.out.println(meuFilme.pegaMedia());
+        System.out.println("Total de avaliacoes " + meuFilme.getTotalDeAvaliacao());
+        System.out.println("Média de avaliações " + meuFilme.pegaMedia());
+//        meuFilme.somaDasAvaliacoes = 10;
+//        meuFilme.totalDeAvaliacao = 1;
+//        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibirFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duracao para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtrar(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVizualizacoes(300);
+        filtro.filtrar(episodio);
+
+
     }
 }
---
+++ Alura/screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,46 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private int episodiosPorTemporadas;
+    private boolean ativa;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporadas() {
+        return episodiosPorTemporadas;
+    }
+
+    public void setEpisodiosPorTemporadas(int episodiosPorTemporadas) {
+        this.episodiosPorTemporadas = episodiosPorTemporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos(){
+        return temporadas * episodiosPorTemporadas * minutosPorEpisodio;
+    }
+
+}
